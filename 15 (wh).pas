7132dd050d5361ed31b531c06dd20b36e8f5c433
Vicketori <144226383+Vicketori@users.noreply.github.com>
2023-10-28 22:21:36 +0300

    Add files via upload

---
+++ 15 (wh).pas
@@ -0,0 +1,20 @@
+﻿{15.	Найти количество цифр числа, их сумму и произведение.}
+var
+  x,y, n, s,pr: integer;
+begin
+  write('Введите число: ');
+  readln(x);
+  pr:=1;
+  x*=10;
+  while x div 10 <> 0 do
+  begin
+    x:= x div 10;
+    y:= x mod 10;
+    s+=y;
+    pr*=y;
+    n+=1;
+  end;
+  writeln('Количество цифр = ', n);
+  writeln('Сумма цифр = ', s);
+  writeln('Произведение цифр = ', pr);
+end.

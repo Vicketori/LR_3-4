7d8a6410e736e7db1f864ca2b6a0f7c1a1840016
Vicketori <144226383+Vicketori@users.noreply.github.com>
2023-10-28 19:30:49 +0300

    Add files via upload

---
+++ 11 (for).pas
@@ -0,0 +1,16 @@
+﻿{11.	Вычислить n!}
+var
+  n,i,k: integer;
+
+begin
+  write('Введите n: ');
+  readln(n);
+  k:=1;
+  if n = 0 then write('n!= ',1)
+  else
+  for i:= 1 to n do
+    begin
+ k*=i;
+  write('n!= ',k)
+  end;
+end.
